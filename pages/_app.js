255cf66527fd872e27696148d09e34db473e140b
Jeffrey Martinez <martinezjf2@gmail.com>
2024-10-28 11:47:12 -0400

    added google analytics to the project

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,24 @@
+import Script from "next/script";
 import "@/styles/scss/styles.scss";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* Google Analytics Script */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-T94Q6D7ZC3"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-T94Q6D7ZC3');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,14 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      screens: {
+        xs: "480px", // Custom breakpoint for extra-small screens
+        sm: "640px", // Small screens
+        md: "768px", // Medium screens
+        lg: "1024px", // Large screens
+        xl: "1280px", // Extra-large screens
+        "2xl": "1536px", // 2x extra-large screens
+      },
     },
   },
   plugins: [require("@tailwindcss/forms")],
