d5ec43575a5046f2dc63d861f42c61aaf662814c
Jeffrey Martinez <martinezjf2@gmail.com>
2024-10-28 12:36:40 -0400

    added a new dependency for google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -5,18 +5,6 @@ export default function Document() {
     <Html lang="en">
       <Head>
         {/* Google Tag Manager */}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-T94Q6D7ZC3"
-        ></script>
-        <script>
-          {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-T94Q6D7ZC3');
-            `}
-        </script>
         <title>Portos | Real Estate Landing Page</title>
       </Head>
       <body>
