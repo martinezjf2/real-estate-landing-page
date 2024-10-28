50150526cc0b56f9dfabdeda9a4749ad937bb6ad
Jeffrey Martinez <martinezjf2@gmail.com>
2024-10-28 11:53:32 -0400

    added a head tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import Script from "next/script";
+import Head from "next/head";
 import "@/styles/scss/styles.scss";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       {/* Google Analytics Script */}
+      <Head>
+        <title>Portos | Real Estate Landing Page</title>
+      </Head>
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-T94Q6D7ZC3"
         strategy="afterInteractive"
