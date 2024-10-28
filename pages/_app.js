5f65a0b4ee0e189dd701484894b34a266c67f1a2
Jeffrey Martinez <martinezjf2@gmail.com>
2024-10-28 12:05:02 -0400

    added a head tag inside the out folder

--- pages/_app.js
+++ pages/_app.js
@@ -9,19 +9,22 @@ export default function App({ Component, pageProps }) {
       <Head>
         <title>Portos | Real Estate Landing Page</title>
       </Head>
+
+      {/* Google Analytics Script */}
       <Script
+        async
         src="https://www.googletagmanager.com/gtag/js?id=G-T94Q6D7ZC3"
-        strategy="afterInteractive"
       />
       <Script id="google-analytics" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-
+          
           gtag('config', 'G-T94Q6D7ZC3');
         `}
       </Script>
+
       <Component {...pageProps} />
     </>
   );
