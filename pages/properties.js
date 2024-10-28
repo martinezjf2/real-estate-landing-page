2b25aad10238694025b181127784ef7d66b32a6c
Jeffrey Martinez <martinezjf2@gmail.com>
2024-10-27 21:34:09 -0400

    added responsive design on the properties section

--- pages/properties.js
+++ pages/properties.js
@@ -2,6 +2,16 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+// Import Swiper and Swiper styles
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import SwiperCore, { Navigation, Pagination } from "swiper";
+
+// Install Swiper modules
+SwiperCore.use([Navigation, Pagination]);
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +26,55 @@ export default function Properties() {
     },
     {
       id: 2,
+      image: "/houses/apartments.jpg",
+      title: "Modern Apartment",
+      location: "New York",
+      price: "$1,200,000",
+    },
+    {
+      id: 3,
+      image: "/houses/apartments.jpg",
+      title: "Modern Apartment",
+      location: "New York",
+      price: "$1,200,000",
+    },
+    {
+      id: 4,
+      image: "/houses/apartments.jpg",
+      title: "Modern Apartment",
+      location: "New York",
+      price: "$1,200,000",
+    },
+    {
+      id: 5,
+      image: "/houses/apartments.jpg",
+      title: "Modern Apartment",
+      location: "New York",
+      price: "$1,200,000",
+    },
+    {
+      id: 6,
+      image: "/houses/apartments.jpg",
+      title: "Modern Apartment",
+      location: "New York",
+      price: "$1,200,000",
+    },
+    {
+      id: 7,
+      image: "/houses/apartments.jpg",
+      title: "Modern Apartment",
+      location: "New York",
+      price: "$1,200,000",
+    },
+    {
+      id: 8,
+      image: "/houses/apartments.jpg",
+      title: "Modern Apartment",
+      location: "New York",
+      price: "$1,200,000",
+    },
+    {
+      id: 9,
       image: "/houses/apartments.jpg",
       title: "Modern Apartment",
       location: "New York",
@@ -36,28 +95,30 @@ export default function Properties() {
     <div>
       <Navbar />
       <div className="px-8 py-16 text-center">
-        <h1 className="text-3xl font-semibold mb-12">Available Properties</h1>
-        <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
-          {propertiesData.map((property) => (
-            <div
-              key={property.id}
-              className="relative overflow-hidden rounded-lg shadow-lg transition-transform transform hover:scale-105"
-            >
-              <Image
-                src={property.image}
-                alt={property.title}
-                width={500}
-                height={300}
-                className="rounded-lg"
-                objectFit="cover"
-              />
-              <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-70 p-4 text-white opacity-0 transition-opacity duration-300 hover:opacity-100">
-                <h2 className="text-lg font-semibold">{property.title}</h2>
-                <p className="text-sm">{property.location}</p>
-                <p className="text-sm font-semibold">{property.price}</p>
+        <div className="py-3">
+          <h1 className="text-3xl font-semibold mb-12">Available Properties</h1>
+          {/* Horizontal Scroll Container */}
+          <div className="flex overflow-x-auto space-x-8 snap-x snap-mandatory md:gap-5 md:grid md:grid-cols-2 lg:grid-cols-3 md:space-x-0 h-auto">
+            {propertiesData.map((property) => (
+              <div
+                key={property.id}
+                className="snap-center flex-shrink-0 w-[75%] md:w-auto relative rounded-lg shadow-lg transition-transform transform"
+              >
+                <Image
+                  src={property.image}
+                  alt={property.title}
+                  width={500}
+                  height={500}
+                  className="rounded-lg w-full"
+                />
+                <div className="absolute bottom-0 left-0 right-0 md:overflow-hidden bg-black bg-opacity-70 p-4 text-white opacity-0 transition-opacity duration-300 hover:opacity-100 rounded-md hover:shadow-lg">
+                  <h2 className="text-lg font-semibold">{property.title}</h2>
+                  <p className="text-sm">{property.location}</p>
+                  <p className="text-sm font-semibold">{property.price}</p>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
 
